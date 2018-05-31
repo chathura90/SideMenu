90415a2230a0ccaeef68d9706b5cf81e39c84a66
Chathura <kck.amcd@gmail.com>
2018-05-31 12:50:52 +0530

    side menu toggle implemented

---
+++ SideMenu/HomeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by hSenid on 5/31/18.
+//  Copyright © 2018 hSenid. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var sideMenuConstraint : NSLayoutConstraint!
+    var sideMenuOpen = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(toggleSideMenu), name: NSNotification.Name("ToggleSideMenu"), object: nil)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func toggleSideMenu(){
+        if sideMenuOpen {
+            sideMenuConstraint.constant = -240
+            sideMenuOpen = false}
+        else{
+            sideMenuConstraint.constant = 0
+            sideMenuOpen = true}
+    }
+
+
+}
+
---
+++ SideMenu/SideMenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuViewController.swift
+//  SideMenu
+//
+//  Created by hSenid on 5/31/18.
+//  Copyright © 2018 hSenid. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuViewController: UITableViewController {
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+
+}
+}
